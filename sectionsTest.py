0210cd9ffcca3c8db04f17e008efbf29544e6ada
andresmit <andresmt@ut.ee>
2015-06-11 14:03:28 +0300

    section managing

--- sectionsTest.py
+++ sectionsTest.py
@@ -3,26 +3,43 @@ __author__ = 'Andres'
 import re
 from pprint import pprint
 
+def sectionsParser(text):
+    pass
+    return {}
+
 if __name__ == '__main__':
     with open("armeenia.txt", encoding='utf-8') as f:
         text = f.read()
 
-
+    level = 0
     entries = re.split("\n=", text)
-    pprint(entries)
+    #pprint(entries)
     print(len(entries))
+    textDict = {}
     titles = []
-    for i in entries:
-        if i.startswith('='):
-            print(i)
-            try:
-                titleEnd = i.index(r'\n')
-                print(titleEnd)
-                titles.append(i[:titleEnd])
-                print(i[:titleEnd])
-            except Exception:
-                titles.append(i)
-    print(titles)
+    sectionTitleRegEx = re.compile('={,1}.+={2,}\n')
+    intro = entries[0]
+    print('intro ', intro)
+    textDict['intro']=intro
+
+    for i in entries[1:]:
+        #TODO: add to {} form
+        #TODO: manage subsections nicely
+        title = re.match(sectionTitleRegEx, i)
+        if title:
+            titleEnd = title.end()
+            print(titleEnd)
+            title = title.group()
+            text = i[titleEnd:]
+
+            print(title, title.count('='))
+            print(text)
+            textDict[]
+            #print(title.count('='))
+
+          #print
+    print(textDict)
+
 """
     pprint(data.splitlines())
     sectionTitles = []
